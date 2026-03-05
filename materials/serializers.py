9c95fb7dc9965521b1ad15fd667d16fb28e91818
GalinaUm <GalinaUmerzakhova@yandex.ru>
2026-03-05 13:51:40 +0300

    style: black and flake8

--- materials/serializers.py
+++ materials/serializers.py
@@ -8,6 +8,7 @@ class LessonSerializer(serializers.ModelSerializer):
         model = Lesson
         fields = "__all__"
 
+
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
--- materials/views.py
+++ materials/views.py
@@ -12,17 +12,21 @@ class CourseViewSet(viewsets.ModelViewSet):
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
 
+
 class LessonListAPIView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
+
 class LessonUpdateAPIView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 class LessonDeleteAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
