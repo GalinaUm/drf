d5bbd38456ea4b737780ffbfd776419efbb2a197
GalinaUm <GalinaUmerzakhova@yandex.ru>
2026-03-08 09:39:27 +0300

    feat: count lessons in course

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Lesson, Course
 
@@ -13,3 +14,14 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(serializers.ModelSerializer):
+    lessons_with_the_same_course_count = SerializerMethodField()
+
+    def get_lessons_with_the_same_course_count(self, lesson):
+        return Lesson.objects.filter(course=lesson).count()
+
+    class Meta:
+        model = Course
+        fields = ('name', 'description', 'lessons_with_the_same_course_count')
